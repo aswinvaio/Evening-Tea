67a8a235e64da041e317aa2212335fc9031ad62b
Aswin V P <aswin@getrealhealth.com>
2017-07-29 20:36:45 +0530

    EOD

--- app/src/main/java/com/example/aswin/eveningtea/DatabaseHelper.java
+++ app/src/main/java/com/example/aswin/eveningtea/DatabaseHelper.java
@@ -1,6 +1,7 @@
 package com.example.aswin.eveningtea;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
@@ -11,23 +12,31 @@ import android.database.sqlite.SQLiteOpenHelper;
 public class DatabaseHelper extends SQLiteOpenHelper{
     public static final String DATABASE_NAME = "EveningTeaDB.db";
     public static final String TABLE_ITEMS = "ITEMS";
-    public static final String ITEMS_COL_1 = "ItemId";
-    public static final String ITEMS_COL_2 = "Name";
-    public static final String ITEMS_COL_3 = "UnitPrice";
-    public static final String ITEMS_COL_4 = "IsHidden";
-    public static final String ITEMS_COL_5 = "";
+    public static final String ITEMS_COL_ItemId = "ItemId";
+    public static final String ITEMS_COL_Name = "Name";
+    public static final String ITEMS_COL_UnitPrice = "UnitPrice";
+    public static final String ITEMS_COL_IsHidden = "IsHidden";
 
     public DatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, 1);
+        SQLiteDatabase db = this.getWritableDatabase();
     }
 
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
-        sqLiteDatabase.execSQL("create table "+TABLE_ITEMS+" (ItemID INTEGER PRIMARY KEY AUTOINCREMENT, Name TEXT, UinitPrice ");
+        sqLiteDatabase.execSQL("CREATE TABLE "+TABLE_ITEMS+" ("+ITEMS_COL_ItemId+" INTEGER PRIMARY KEY AUTOINCREMENT, "+ITEMS_COL_Name+" TEXT NOT NULL, "+ITEMS_COL_UnitPrice+" INT NOT NULL, "+ITEMS_COL_IsHidden+" INT NOT NULL);");
+        sqLiteDatabase.execSQL("INSERT INTO "+TABLE_ITEMS+" ("+ITEMS_COL_Name+", "+ITEMS_COL_UnitPrice+", "+ITEMS_COL_IsHidden+") VALUES ('Coffee', 10, 0),('Tea', 8, 0),('Black Tea', 5, 0),('Boost', 10, 0),('Horlicks', 10, 0),('Cake', 10, 0),('Puffs', 12, 0),('Vada', 8, 0),('Ada', 7, 0),('Pazham', 10, 0),('Biscut', 5, 0),('Sweet Roll', 10, 0),('Madal', 10, 0),('Bun', 5, 0),('Brown Bun', 10, 0),('Lime', 10, 0)");
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS "+TABLE_ITEMS);
+        onCreate(sqLiteDatabase);
+    }
 
+    public Cursor getAllData(){
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor res = db.rawQuery("SELECT * FROM "+TABLE_ITEMS, null);
+        return res;
     }
 }
--- app/src/main/java/com/example/aswin/eveningtea/EveningTeaMain.java
+++ app/src/main/java/com/example/aswin/eveningtea/EveningTeaMain.java
@@ -1,10 +1,13 @@
 package com.example.aswin.eveningtea;
 
+import android.database.Cursor;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.StringBuilderPrinter;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -13,13 +16,15 @@ import android.widget.Toast;
 
 public class EveningTeaMain extends AppCompatActivity {
 
+    DatabaseHelper myDB;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_evening_tea_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+        myDB = new DatabaseHelper(this);
     }
 
     @Override
@@ -35,11 +40,37 @@ public class EveningTeaMain extends AppCompatActivity {
 
         }
         if(item.getItemId() == R.id.navigation_items){
-
+            read();
         }
         if(item.getItemId() == R.id.navigation_itemPurchases){
 
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    public void read(){
+        Cursor res = myDB.getAllData();
+        if(res.getCount() == 0){
+            showMessage("title", "no data");
+            return;
+        }
+
+        StringBuffer buffer = new StringBuffer();
+        while(res.moveToNext()){
+            buffer.append("ItemId : "+res.getString(0)+"\n");
+            buffer.append("Name : "+res.getString(1)+"\n");
+            buffer.append("UnitPrice : "+res.getString(2)+"\n");
+            buffer.append("IsHidden : "+res.getString(3)+"\n\n");
+        }
+
+        showMessage("Data",buffer.toString());
+    }
+
+    public void showMessage(String title, String Message){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setCancelable(true);
+        builder.setTitle(title);
+        builder.setMessage(Message);
+        builder.show();
     }
 }
